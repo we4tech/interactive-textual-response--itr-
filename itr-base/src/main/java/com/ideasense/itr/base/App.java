2a3df0956c2c181a05028cb0b755cd92094e842f
nhm tanveer hossain khan <hasan83bd@gmail.com>
2007-05-26 08:11:27 +0000

    ITR project setup completed.

---
+++ itr-base/src/main/java/com/ideasense/itr/base/App.java
@@ -0,0 +1,13 @@
+package com.ideasense.itr.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
